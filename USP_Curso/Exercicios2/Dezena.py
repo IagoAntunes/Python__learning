568cff11dcf7560c9f90201a0da6d09b38f04c48
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-28 18:43:38 -0300

    Iago Antunes Ferreira

---
+++ USP_Curso/Exercicios2/Dezena.py
@@ -0,0 +1,8 @@
+
+
+num = int(input("Digite o Numero: "))
+
+
+dezena = (num // 10) % 10
+
+print("O digito das dezenas é : ",dezena)
---
+++ USP_Curso/Exercicios2/Fatura.py
@@ -0,0 +1,10 @@
+
+
+
+nome = input("Nome do cliente: ")
+dv = int(input("Dia do vencimento: "))
+mv = input("Digite o mes de vencimento: ")
+fatura = input("Fatura: ")
+
+print("Olá,",nome)
+print("A sua fatura com vencimento em",dv,"de",mv,"no valor de R$",fatura,"está fechada.")
