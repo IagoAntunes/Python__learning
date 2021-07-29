4e6b6bc99c4c685adaf33fc670cb9a7c8e35aab0
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-28 21:07:22 -0300

    Iago Antunes Ferreira

---
+++ USP_Curso/Semana3/Exercicio5.py
@@ -0,0 +1,9 @@
+num = int(input("Digite um numero: "))
+num2 = int(input("Digite segundo numero: "))
+num3 = int(input("Digite terceiro numero: "))
+
+
+if(num < num2 and num2 < num3):
+    print("crescente")
+else:
+    print("não está em ordem crescente")
