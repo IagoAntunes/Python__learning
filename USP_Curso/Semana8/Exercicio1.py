f8b15157236bf03cf526d8d9e81889e2c960ef49
Iago Antunes <iagoantunes.f@gmail.com>
2021-07-29 03:35:17 -0300

    Iago Antunes Ferreira

--- USP_Curso/Semana8/Exercicio1.py
+++ USP_Curso/Semana8/Exercicio1.py
@@ -1,18 +1,9 @@
 
-def remove_repetidos(lista,lists2):
-    j = 0
-    for i in range(0,len(lista)):
-        while(j < len(lista2)):
-            if(lista[i] == lista[j]):
-                del lista2[j]
-            j = j + 1
-
-
-lista = [2, 4, 2, 2, 3, 3, 1]
-lista2 = lista[:]
-remove_repetidos(lista,lista2)
-lista2.sort()
-
-
+def remove_repetidos(lista):
+    lista2 = []
+    for i in lista :
+        if i not in lista2 :
+            lista2.append(i)
+    return sorted(lista2)
 
                 
